2aa035e1cf1deaed1f1a259bbde0bcc4cdfbb766
Noskarant <tutomod@gmail.com>
2026-07-08 20:02:51 +0200

    Separate GitHub Pages build cache

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const isGitHubPages = process.env.GITHUB_PAGES === "true";
 
 const nextConfig: NextConfig = {
   output: isGitHubPages ? "export" : undefined,
+  distDir: isGitHubPages ? ".next-github-pages" : ".next",
   basePath: isGitHubPages ? "/apexliner" : undefined,
   assetPrefix: isGitHubPages ? "/apexliner/" : undefined,
   trailingSlash: isGitHubPages,
